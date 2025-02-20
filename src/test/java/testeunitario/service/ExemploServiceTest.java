79338496d7a0aaedcf58f4ae1f57e747dbf21eb9
Josias Carneiro Teixeira de Araújo <202215020021@ad.ifpb.edu.br>
2025-02-20 20:24:19 -0300

    Adicionando plugins da aula

--- src/test/java/testeunitario/service/ExemploServiceTest.java
+++ src/test/java/testeunitario/service/ExemploServiceTest.java
@@ -3,7 +3,9 @@ package testeunitario.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class ExemploServiceTest {
     
     @Test
